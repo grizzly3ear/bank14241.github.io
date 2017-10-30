8db9e90dfa470e4d26105a1aaa42d12476856104
GBear <b.a.n.k.2_11-26@hotmail.com>
2017-10-31 01:13:54 +0700

    update
    time set back to 0 when press stop

--- js/script.js
+++ js/script.js
@@ -9,11 +9,12 @@ function playAudio(task){
             }
         }
         for (var index = 0; index < playlist.length; index++) {
-            $('.audio'+playlist[index]).trigger('play');
+            $('#audio'+playlist[index]).trigger('play');
         }
     } else if(task == 'stop'){
         for (var index = 0; index < playlist.length; index++) {
-            $('.audio'+playlist[index]).trigger('pause');
+            $('#audio'+playlist[index]).trigger('pause');
+            $('#audio'+playlist[index]).prop('currentTime', 0);
         }
         playlist = [];
     }
@@ -23,14 +24,16 @@ function stopCustom(){
     let x = $('#audioStopInput').val();
     for (var index = 0; index < x.length; index++) {
         if (x.charAt(index) != ' ') {
-            $('.audio'+x.charAt(index)).trigger('pause');
+            let id = x.charAt(index);
+            $('#audio'+id).trigger('pause');
+            $('#audio'+id).prop('currentTime', 0);
         }        
     }
 }
 
 function fadeOut(id){
-    $('.audio'+id).animate({volume: 0}, 1000);
+    $('#audio'+id).animate({volume: 0}, 1000);
     setTimeout(function(){
-        $('.audio'+id).trigger('pause');
+        $('#audio'+id).trigger('pause');
     }, 1000);
 }
