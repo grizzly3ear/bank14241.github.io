0215a6b56ba10c69bb5188c10be0c151c8aa8a21
GrizzlyBear <udomphol2@gmail.com>
2017-10-31 02:25:06 +0700

    fix bug
    fix bug when over 10 index

--- js/script.js
+++ js/script.js
@@ -1,40 +1,34 @@
 let playlist = [];
 
-function playAudio(task){
+function playAudio(task) {
     if (task == 'play') {
         let x = $('#audioInput').val();
-        playlist = [];
-        for (var index = 0; index < x.length; index++) {
-            if (x.charAt(index) != ' ') {
-                playlist.push(x.charAt(index));
-            }
-        }
+        playlist = x.split(" ");
         for (var index = 0; index < playlist.length; index++) {
-            $('#audio'+playlist[index]).trigger('play');
+            $('#audio' + playlist[index]).trigger('play');
         }
-    } else if(task == 'stop'){
+    } else if (task == 'stop') {
         for (var index = 0; index < playlist.length; index++) {
-            $('#audio'+playlist[index]).trigger('pause');
-            $('#audio'+playlist[index]).prop('currentTime', 0);
+            $('#audio' + playlist[index]).trigger('pause');
+            $('#audio' + playlist[index]).prop('currentTime', 0);
         }
         playlist = [];
     }
 }
 
-function stopCustom(){
+function stopCustom() {
     let x = $('#audioStopInput').val();
-    for (var index = 0; index < x.length; index++) {
-        if (x.charAt(index) != ' ') {
-            let id = x.charAt(index);
-            $('#audio'+id).trigger('pause');
-            $('#audio'+id).prop('currentTime', 0);
-        }        
+    let stoplist = x.split(" ");
+    for (var index = 0; index < stoplist.length; index++) {
+        $('#audio' + stoplist[index]).trigger('pause');
+        $('#audio' + stoplist[index]).prop('currentTime', 0);
+
     }
 }
 
-function fadeOut(id){
-    $('#audio'+id).animate({volume: 0}, 1000);
-    setTimeout(function(){
-        $('#audio'+id).trigger('pause');
+function fadeOut(id) {
+    $('#audio' + id).animate({ volume: 0 }, 1000);
+    setTimeout(function () {
+        $('#audio' + id).trigger('pause');
     }, 1000);
 }
